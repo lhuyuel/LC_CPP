522b76f1f4a2ef15598e0e2803954c3588e09bca
lhuyuel <hihuyue@gmail.com>
2016-07-22 10:50:39 -0400

    [345. Reverse Vowels of a String][Accepted]auto commit

--- 345-Reverse-Vowels-of-a-String/solution.cpp
+++ 345-Reverse-Vowels-of-a-String/solution.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     // vowels are: a e i o u ?
     string reverseVowels(string s) {
-        unordered_map<char,bool> vowelTable = {
-            {'a',true},
-            {'e',true},
-            {'i',true},
-            {'o',true},
-            {'u',true},
-            {'A',true},
-            {'E',true},
-            {'I',true},
-            {'O',true},
-            {'U',true}};
+        bool vowelTable[52];
+        vowelTable['a'-'A'] = true;
+        vowelTable['e'-'A'] = true;
+        vowelTable['i'-'A'] = true;
+        vowelTable['o'-'A'] = true;
+        vowelTable['u'-'A'] = true;
+        vowelTable['A'-'A'] = true;
+        vowelTable['E'-'A'] = true;
+        vowelTable['I'-'A'] = true;
+        vowelTable['O'-'A'] = true;
+        vowelTable['U'-'A'] = true;
         
         for (int i = 0, j = s.size()-1; i < j;) {
-            while (vowelTable.find(s[i]) == vowelTable.end()) ++i;
-            while (vowelTable.find(s[j]) == vowelTable.end()) --j;
-            if (i < j) swap(s[i++],s[j--]);
-            else break;
+            while (i<j && !vowelTable[s[i]-'A']) ++i;
+            while (i<j && !vowelTable[s[j]-'A']) --j;
+            swap(s[i++],s[j--]);
         }
         return s;
     }

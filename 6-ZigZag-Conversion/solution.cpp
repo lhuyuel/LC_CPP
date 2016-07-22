d6ec45cdaa681623d651f8108d5e700f1626618b
lhuyuel <hihuyue@gmail.com>
2016-07-22 15:10:27 -0400

    [6. ZigZag Conversion][Time Limit Exceeded]auto commit

---
+++ 6-ZigZag-Conversion/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int len = s.size();
+        vector<string> rows(numRows,"");
+        for (int i = 0; i < len; ) {
+            int j = 0;
+            while (j < numRows-1 && i < len) 
+                rows[j++] += s[i++];
+            j = numRows - 1;
+            while (j > 0 && i < len)
+                rows[j--] += s[i++];
+        }
+        
+        // cat all the rows
+        string result = "";
+        for (auto row : rows) 
+            result += row;
+        return result;
+    }
+};

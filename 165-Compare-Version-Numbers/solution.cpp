7b945c69561c3ce0a661ece816e0ac78a21d2506
lhuyuel <hihuyue@gmail.com>
2016-07-23 17:20:18 -0400

    [165. Compare Version Numbers][Runtime Error]auto commit

--- 165-Compare-Version-Numbers/solution.cpp
+++ 165-Compare-Version-Numbers/solution.cpp
@@ -3,11 +3,15 @@ public:
     // example: 1.5 < 1.11
 
     int compareVersion(string version1, string version2) {
-            for (int i = 0, j = 0; i< version1.size() && j < version2.size(); ) {
+            int i = 0, j = 0;
+            while (i< version1.size() || j < version2.size()) {
                 string v1 = "", v2 = "";
                 while (version1[i] != '.' && i < version1.size()) v1 += version1[i++];
                 while (version2[j] != '.' && j < version2.size()) v2 += version2[j++];
-                ++i, ++j;
+                ++i;
+                ++j;
+                if (i > version1.size()+1) v1 = "0";
+                if (j > version1.size()+1) v2 = "0";
                 if (stoi(v1) > stoi(v2)) return 1;
                 else if (stoi(v1) < stoi(v2)) return -1;
             }

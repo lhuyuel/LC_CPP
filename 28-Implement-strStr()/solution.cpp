6b47456c9fcdd49d1f61cee2893c9d9c06d02a46
lhuyuel <hihuyue@gmail.com>
2016-07-24 12:56:35 -0400

    [28. Implement strStr()][Time Limit Exceeded]auto commit

--- 28-Implement-strStr()/solution.cpp
+++ 28-Implement-strStr()/solution.cpp
@@ -13,9 +13,11 @@ public:
 
     int strStr(string haystack, string needle) {
         if (haystack == needle || needle == "") return 0;
+        else if(haystack.size() < needle.size() ) return -1;
         
         for (int i = 0; i < haystack.size(); ) {
             while (haystack[i] != needle[0]) ++i;
+            if (i > haystack.size() -1 ) return -1;
             int j = 0;
             for (; j < haystack.size()-i && j < needle.size(); ++j ) { 
                 if( haystack[j+i] != needle[j]) 

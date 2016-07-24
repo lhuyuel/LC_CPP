ddf8a06f13ab4e18ce32994dc727525a56a77ac3
lhuyuel <hihuyue@gmail.com>
2016-07-24 12:00:46 -0400

    [125. Valid Palindrome][Compile Error]auto commit

--- 125-Valid-Palindrome/solution.cpp
+++ 125-Valid-Palindrome/solution.cpp
@@ -8,8 +8,8 @@ public:
     bool isPalindrome(string s) {
         if (s.size() == 0) return true;
         for (int i = 0, j = s.size()-1; i < j && i < s.size() && j < s.size();) {
-            while (!isapha(s[i]) && !isdigit(s[i])) && i < s.size()) ++i;
-            while (!isapha(s[j]) && !isdigit(s[j])) && j < s.size()) --j;
+            while (!isalpha(s[i]) && !isdigit(s[i])) && i < s.size()) ++i;
+            while (!isalpha(s[j]) && !isdigit(s[j])) && j < s.size()) --j;
             if (i < s.size() && j > 0 ) {
                 if (isalpha(s[i]) && isalpha(s[j]) ){
                     if (abs(s[i]-s[j]) != 32 && abs(s[i]-s[j]) != 0)

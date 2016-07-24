3781b4cb4ced280c1470b84975f6a34be0dfe0e1
lhuyuel <hihuyue@gmail.com>
2016-07-24 12:00:29 -0400

    [125. Valid Palindrome][Compile Error]auto commit

--- 125-Valid-Palindrome/solution.cpp
+++ 125-Valid-Palindrome/solution.cpp
@@ -4,13 +4,13 @@ public:
 // "race a car"
 // Aba
 // "0P"
-
+//a.
     bool isPalindrome(string s) {
         if (s.size() == 0) return true;
         for (int i = 0, j = s.size()-1; i < j && i < s.size() && j < s.size();) {
-            while ((s[i]==' ' || s[i] == ',') && i < s.size()) ++i;
-            while ((s[j]==' ' || s[j] == ',') && j < s.size()) --j;
-            if( i < s.size() && j > 0 )
+            while (!isapha(s[i]) && !isdigit(s[i])) && i < s.size()) ++i;
+            while (!isapha(s[j]) && !isdigit(s[j])) && j < s.size()) --j;
+            if (i < s.size() && j > 0 ) {
                 if (isalpha(s[i]) && isalpha(s[j]) ){
                     if (abs(s[i]-s[j]) != 32 && abs(s[i]-s[j]) != 0)
                         return false;
@@ -20,7 +20,6 @@ public:
                         return false;
             }
             ++i, --j;
-
         }
         return true;
     }

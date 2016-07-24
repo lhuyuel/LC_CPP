2814df6776fc3ef6226fa1d23e33858b9822491b
lhuyuel <hihuyue@gmail.com>
2016-07-24 12:31:02 -0400

    [28. Implement strStr()][Runtime Error]auto commit

---
+++ 28-Implement-strStr()/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // could use KMP algorithm
+    
+    // haystack = "abcdfg", needle = "cde"
+    // abcd      e    | expected -1
+    // ""    "a"
+    //  "abcdefcdg"     cdg
+    // Brute Force
+    int strStr(string haystack, string needle) {
+        if (haystack == needle) return 0;
+        int pre = 0;
+        for (int i = 0; i < haystack.size(); ++i ) {
+            while (haystack[i] != needle[0]) ++i;
+            pre = i + 1;
+            int j = 0;
+            for (; i< haystack.size() && j < needle.size(); ++j ) { 
+                if( haystack[i++] != needle[j]) 
+                    break;
+            }
+            if (j == needle.size()) return pre-1;
+            // rest i
+            i = pre;
+        }
+        return -1;
+    }
+};

2a3804c92f9f3b1642363b3f1864f0abce47ae81
lhuyuel <hihuyue@gmail.com>
2016-07-26 14:30:31 -0400

    [235. Lowest Common Ancestor of a Binary Search Tree][Compile Error]auto commit

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.cpp
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.cpp
@@ -31,7 +31,7 @@ public:
         if (!root) return nullptr;
         if (root->val >= p->val && root->val <= q->val || root->val >= q->val && root->val <= p->val)
             return root;
-        else if (root->val > p) lowestCommonAncestor(root->left, p, q);
+        else if (root->val > p->val) lowestCommonAncestor(root->left, p, q);
         else lowestCommonAncestor(root->right, p, 1);
     }
 };

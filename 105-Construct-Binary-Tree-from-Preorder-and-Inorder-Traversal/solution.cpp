0d9d96ef6d41cc3cad3a89383af12d2ae5f67a67
lhuyuel <hihuyue@gmail.com>
2016-07-29 13:48:22 -0400

    [105. Construct Binary Tree from Preorder and Inorder Traversal][Runtime Error]auto commit

---
+++ 105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int preOrderIdx = 0;
+    int getInOrderIdx(vector<int> &inorder, int x, int start, int end){
+        for(int i = start; i <= end; ++i){
+            if( inorder[i] == x)
+                return i;
+        }
+        return -1;
+    }
+    
+    TreeNode* buildHelper(vector<int>& preorder, vector<int>& inorder, int start, int end) {
+        
+        
+        if (start > end) return nullptr;
+        TreeNode *node = new TreeNode(preorder[preOrderIdx++]);
+        if (start == end) return node;
+        static int idx = getInOrderIdx(inorder, preorder[preOrderIdx++], start, end);
+        node->left = buildHelper(preorder,inorder, start, idx-1);
+        node->right = buildHelper(preorder, inorder, idx +1, end);
+        return node;
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return buildHelper(preorder,inorder, 0, preorder.size()-1);
+    }
+};

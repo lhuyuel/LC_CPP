9e01b0120393f30037a68be2aa093c2f31c9f8f6
lhuyuel <hihuyue@gmail.com>
2016-07-30 11:22:38 -0400

    [96. Unique Binary Search Trees][Runtime Error]auto commit

---
+++ 96-Unique-Binary-Search-Trees/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    // f(n) = f(0)*f(n-1) + f(1) *f(n-2) +...+ f(n-1)*f(0)
+    // f(0) = 1, f(1) = 1
+    // f(2) = 1*1 + 1*1 = 2
+    // test case: n =3:
+    //   k = 2
+    // catlan[2] = 2
+    // c[3] = c[0]*c[2] =2             c[3] = 2 + c[1]*c[1] =3      c[3] = 3 + c[2]*c[0] = 5
+    
+    
+    int numTrees(int n) {
+        vector<int> catlan(n,0);
+        catlan[0] = catlan[1] = 1;
+        
+        for(int k = 2; k <= n; ++k) {
+            for (int i = 0; i < k; ++i) {
+                catlan[k] += (catlan[i] * catlan[k-i-1]);
+            }
+        }
+        return catlan[n];
+    }
+};

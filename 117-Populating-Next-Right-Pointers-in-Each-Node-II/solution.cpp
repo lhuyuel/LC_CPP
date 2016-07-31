b6a0bab6335c3c8faa3a607e6954deffec387c39
lhuyuel <hihuyue@gmail.com>
2016-07-31 00:59:10 -0400

    [117. Populating Next Right Pointers in Each Node II][Compile Error]auto commit

---
+++ 117-Populating-Next-Right-Pointers-in-Each-Node-II/solution.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode *findFirst(TreeNode start) {
+        while (start) {
+            if (start->left) return start->left;
+            if (start->right) return start->right;
+            start = start->next;
+        }
+        return nullptr;
+    }
+
+    void connect(TreeLinkNode *root) {
+        if (!root) return nullptr;
+        TreeNode *levelStart = root, *cur = root;
+        root->next = nullptr;
+        while (levelStart) {
+            while (cur) {
+                if (cur->left) {
+                    if (cur->right) {
+                        cur->left->next = cur->right;
+                        cur->right->next = findFirst(cur);
+                    }
+                    else 
+                        cur->left->next = findFirst(cur);
+                }
+                cur = cur->next;
+            }
+            if (!cur) {
+                cur = levelStart;
+                levelStart = findFirst(levelStart);
+            }
+        }
+    }
+};

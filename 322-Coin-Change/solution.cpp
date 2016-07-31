5f0528654e21894e095859b271b9deabf9fbed3c
lhuyuel <hihuyue@gmail.com>
2016-07-31 19:57:10 -0400

    [322. Coin Change][Runtime Error]auto commit

--- 322-Coin-Change/solution.cpp
+++ 322-Coin-Change/solution.cpp
@@ -5,18 +5,23 @@ public:
 coins = [1, 2, 5], amount = 11
 */
     int coinChange(vector<int>& coins, int amount) {
+       //if (amount < coins[0]) return -1;    
        int n = coins.size();
-       vector<int> f(amount+1, 0);
+       vector<int> f(amount+1, -1);
+       f[0] = 0;
        for (int i = 0; i < n; ++i) 
             f[coins[i]] = 1;
        
        for (int j = 1; j <= amount; ++ j) {
             int minNum = INT_MAX;
             for (int i = 0; i < n; ++i) {
-                 if ( j >= coins[i] && f[j-coins[i]] + 1 < minNum)
-                     minNum =  f[j-coins[i]] + 1;
+                 if ( j >= coins[i] && f[j-coins[i]] != -1) { 
+                    if (f[j-coins[i]] + 1 < minNum)
+                        minNum =  f[j-coins[i]] + 1;
+                 }
             }
-            f[j] = minNum;
+            if (minNum != INT_MAX)
+                f[j] = minNum;
        }
        return f[amount];
     }

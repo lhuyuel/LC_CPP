158edecac832970e72bd1805e815404cfd6ca819
lhuyuel <hihuyue@gmail.com>
2016-08-01 10:42:43 -0400

    [169. Majority Element][Accepted]auto commit

--- 169-Majority-Element/solution.cpp
+++ 169-Majority-Element/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-/* 方法1： sort
+/* 方法1： sort     O(nlogn)
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         return nums[nums.size()/2];
     }
 */
-
+    // Boyer-Moore    O(n)
     int majorityElement(vector<int> &num) {
         int maj;
         for (int count = 0, i = 0; i < num.size() && count <= num.size()/2; i++){

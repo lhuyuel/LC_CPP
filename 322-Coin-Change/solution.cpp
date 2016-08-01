d513a580677f973171424ea27ba9a5d6015b7374
lhuyuel <hihuyue@gmail.com>
2016-07-31 20:01:16 -0400

    [322. Coin Change][Compile Error]auto commit

--- 322-Coin-Change/solution.cpp
+++ 322-Coin-Change/solution.cpp
@@ -5,7 +5,9 @@ public:
 coins = [1, 2, 5], amount = 11
 */
     int coinChange(vector<int>& coins, int amount) {
-       //if (amount < coins[0]) return -1;    
+       if (amount == 0) return 0;
+       coins.sort(coins.begin(),coins.end());
+       if (amount < coins[0]) return -1;    
        int n = coins.size();
        vector<int> f(amount+1, -1);
        f[0] = 0;

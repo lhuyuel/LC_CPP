dac94f41266d8adcec2042079519040dcbce024b
lhuyuel <hihuyue@gmail.com>
2016-08-01 11:00:34 -0400

    [231. Power of Two][Accepted]auto commit

--- 231-Power-of-Two/solution.cpp
+++ 231-Power-of-Two/solution.cpp
@@ -1,7 +1,13 @@
 class Solution {
 public:
+/*  一般解法
     bool isPowerOfTwo(int n) {
         if (n < 1) return false;
         return (!(n&(n-1)));
+    }
+*/ 
+    bool isPowerOfTwo(int n) {
+        bitset<64> b(n);
+        return b.count()==1;
     }
 };

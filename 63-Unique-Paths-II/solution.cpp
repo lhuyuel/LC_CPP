bd00fdfef57c7e0ce3670a211a54c448aa5fef96
lhuyuel <hihuyue@gmail.com>
2016-08-01 00:33:39 -0400

    [63. Unique Paths II][Runtime Error]auto commit

--- 63-Unique-Paths-II/solution.cpp
+++ 63-Unique-Paths-II/solution.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        if (obstacleGrid[0][0]) return 0;
+       
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
+        if (obstacleGrid[0][0]) return 0;
         
         vector<int> dp(m+1,0);
-    
-        for (int i = 1; i < n; ++i) 
+        dp[1] = 1;
+        for (int i = 0; i < n; ++i) 
             for (int j = 1; j < m; ++j) {
-                dp[j] = obstacleGrid[i-1][j-1]) ? 0 : dp[j] + dp[j-1];
+                dp[j] = obstacleGrid[i-1][j-1] ? 0 : dp[j] + dp[j-1];
             }
         return dp[m];
     }

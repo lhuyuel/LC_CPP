58d4d77591eb4069b96febc791fdce516b51a23f
lhuyuel <hihuyue@gmail.com>
2016-07-31 23:32:04 -0400

    [322. Coin Change][Accepted]auto commit

--- 322-Coin-Change/solution.cpp
+++ 322-Coin-Change/solution.cpp
@@ -5,13 +5,18 @@ public:
 coins = [1, 2, 5], amount = 11
 */
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> f(amount+1, INT_MAX-1);
-        f[0] = 0;
-       
-        for (int j = 0; j < coins.size(); ++j)
-            for (int i = coins[j]; i <= amount; ++i) {
-                f[i] = min(f[i-coins[j]] + 1, f[i]);
-            }    
-        return f[amount] == INT_MAX-1 ? -1 : f[amount];
+        //int Max = amount + 1;
+        vector<int> dp(amount + 1, INT_MAX-1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            int minNum = INT_MAX-1;
+            for (int j = 0; j < coins.size(); ++j) {
+                if (coins[j] <= i) {
+                    minNum = min(minNum, dp[i - coins[j]] + 1);
+                }
+            }
+            dp[i] = minNum;
+        }
+        return dp[amount] == INT_MAX-1 ? -1 : dp[amount];
     }
 };

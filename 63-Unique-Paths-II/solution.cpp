b200400dffb81d51f442cebf7f7463c8d7a91f08
lhuyuel <hihuyue@gmail.com>
2016-08-01 00:28:48 -0400

    [63. Unique Paths II][Compile Error]auto commit

--- 63-Unique-Paths-II/solution.cpp
+++ 63-Unique-Paths-II/solution.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0]) return 0;
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        vector<int> dp(m+1,1);
         
-        for (int j = 1; j < m; ++j) 
-                if (!obstacleGrid[0][m]) dp[j] = 0;
-
+        vector<int> dp(m+1,0);
+    
         for (int i = 1; i < n; ++i) 
             for (int j = 1; j < m; ++j) {
-                if (!obstacleGrid[n][m]) dp[j] += dp[j-1];
-                else dp[j] = 0;
+                dp[j] = obstacleGrid[i-1][j-1]) ? 0 : dp[j] + dp[j-1];
             }
         return dp[m];
     }

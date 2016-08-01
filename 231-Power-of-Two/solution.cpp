a07052f1b6274c2e2613ecb75ff291edd482f5f9
lhuyuel <hihuyue@gmail.com>
2016-08-01 10:56:35 -0400

    [231. Power of Two][Accepted]auto commit

--- 231-Power-of-Two/solution.cpp
+++ 231-Power-of-Two/solution.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n < 1) return false;
+        if (n == 1) return true;
+        else if (n < 1 || (n&1)) return false;
         return (!(n&(n-1)));
     }
 };

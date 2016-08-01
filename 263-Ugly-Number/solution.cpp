683e56a9bbf8418058e4d81982859ef287812de6
lhuyuel <hihuyue@gmail.com>
2016-08-01 12:06:03 -0400

    [263. Ugly Number][Accepted]auto commit

--- 263-Ugly-Number/solution.cpp
+++ 263-Ugly-Number/solution.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     // DP: f(n) = f(n/5) || f(n/3) || f(n/2)
     bool isUgly(int num) {
-        vector<int> factors = {2, 3, 5}
+        vector<int> factors = {2, 3, 5};
         if (num == 0) return false;
         for (auto factor : factors) {
-            if (num%factor == 0) 
-                while (num) num /= factor;
+            while (num%factor == 0) num /= factor;
         }
         return num == 1;
     }

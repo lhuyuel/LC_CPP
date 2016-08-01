07b3dbbc220a1f8a267b78263ad1e0d6d0893e16
lhuyuel <hihuyue@gmail.com>
2016-08-01 10:41:44 -0400

    [169. Majority Element][Accepted]auto commit

--- 169-Majority-Element/solution.cpp
+++ 169-Majority-Element/solution.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+/* 方法1： sort
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         return nums[nums.size()/2];
+    }
+*/
+
+    int majorityElement(vector<int> &num) {
+        int maj;
+        for (int count = 0, i = 0; i < num.size() && count <= num.size()/2; i++){
+            if (count == 0){
+                maj = num[i];
+                count++;
+            }
+            else num[i] == maj ? count++ : count--;
+        }
+        return maj;
     }
 };

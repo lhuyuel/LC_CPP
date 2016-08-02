57dddb8dc8539c542587bd14e6d29fb0bd18e9c6
lhuyuel <hihuyue@gmail.com>
2016-08-02 12:21:55 -0400

    [219. Contains Duplicate II][Accepted]auto commit

--- 219-Contains-Duplicate-II/solution.cpp
+++ 219-Contains-Duplicate-II/solution.cpp
@@ -4,9 +4,8 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> table;
         for (int i = 0; i < nums.size(); ++i) {
-            if (table.find(nums[i]) != table.end()) {
+            if (table.find(nums[i]) != table.end())
                 if ((i-table[nums[i]]) <= k) return true;
-            }
             table[nums[i]] = i;
         }
         return false;

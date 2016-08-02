bb9d9b9e2f3f56f30cb90a737adee9f328b52b1b
lhuyuel <hihuyue@gmail.com>
2016-08-02 11:20:51 -0400

    [299. Bulls and Cows][Runtime Error]auto commit

--- 299-Bulls-and-Cows/solution.cpp
+++ 299-Bulls-and-Cows/solution.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     // 这个不是mastermind吗？
     string getHint(string secret, string guess) {
-        int countA = 0, countTotal = 0;
         vector<int> hashTable(10,0);
+        int countA = 0, countTotal = 0;
         for (int i = 0; i < secret.size(); ++i) {
             if (secret[i] == guess[i]) 
                 ++countA;

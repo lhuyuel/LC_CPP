d54c951bb6e0cd0b3ecc8422beb513d9200173e3
lhuyuel <hihuyue@gmail.com>
2016-08-02 15:21:03 -0400

    [88. Merge Sorted Array][Accepted]auto commit

--- 88-Merge-Sorted-Array/solution.cpp
+++ 88-Merge-Sorted-Array/solution.cpp
@@ -4,7 +4,7 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         for (int i = m+n-1; i >= 0; --i) {
             if (n <= 0) break;
-            if (m <= 0) {
+            if (m <= 0)
                 nums1[i] = nums2[--n];
             else
                 nums1[i] = (nums1[m-1] > nums2[n-1]) ? nums1[--m] : nums2[--n];

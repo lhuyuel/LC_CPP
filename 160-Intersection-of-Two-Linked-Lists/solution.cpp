0aa2134d6b0f32646412ec8f9a7c6c7d9dad77b8
lhuyuel <hihuyue@gmail.com>
2016-08-02 14:05:33 -0400

    [160. Intersection of Two Linked Lists][Accepted]auto commit

--- 160-Intersection-of-Two-Linked-Lists/solution.cpp
+++ 160-Intersection-of-Two-Linked-Lists/solution.cpp
@@ -8,6 +8,8 @@
  */
 class Solution {
 public:
+    // 思路是l1走到第一个list结尾后转到第二个list的开头，l2走到第二个list结尾后转到第一个list开头
+    // 当l1于l2相等时返回。对于没有intersection的情况，会返回nullptr因为l1, l2都会等于结尾null。
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if  (!headA || !headB) return nullptr;
         ListNode *l1 = headA, *l2 = headB;

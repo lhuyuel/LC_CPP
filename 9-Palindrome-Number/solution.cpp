f5fbca87c7050d235165f1164a70539d1500c830
lhuyuel <hihuyue@gmail.com>
2016-08-02 09:35:27 -0400

    [9. Palindrome Number][Accepted]auto commit

--- 9-Palindrome-Number/solution.cpp
+++ 9-Palindrome-Number/solution.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-         if( x < 0 )
-             return false;
-         if( x < 10)
-             return true;
-         int digit = 0;
-         int tmp = x;
-         while( tmp >= 1 )
-         {
-             tmp/=10;
-             ++digit;
-         }
-         for(int i = 0; i < digit/2; ++i)
-         {
-             if( ( (int)(x/pow(10,i) )%10) != (int)( x/pow(10,digit-i-1) )%10 )
-                 return false;
-         }
-        return true;
+        if (x < 0) return false;
+        int num = x, sum = 0;
+        while (num > 0){
+            sum = sum*10 + num%10;
+            num /= 10;
+        }
+        return sum == x;
     }
 };

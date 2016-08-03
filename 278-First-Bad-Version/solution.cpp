100580100118a4480c750931ffca8bc8a87b8fc1
lhuyuel <hihuyue@gmail.com>
2016-08-02 22:43:08 -0400

    [278. First Bad Version][Time Limit Exceeded]auto commit

--- 278-First-Bad-Version/solution.cpp
+++ 278-First-Bad-Version/solution.cpp
@@ -6,16 +6,11 @@ public:
     int firstBadVersion(int n) {
         if (n == 1) return 1;
         int start = 1, end = n, mid = 0;
-        while (1) {
+        while (start != end) {
             mid = (end - start)/2 + start;
-            if (isBadVersion(mid)) {
-                if (isBadVersion(mid+1)) start = mid +1;
-                else return mid;
-            }
-            else { 
-                if (isBadVersion(mid -1)) return mid-1;
-                else end = mid-1;
-            }
+            if (isBadVersion(mid)) end = mid;
+            else start = mid;
         }
+        return start;
     }
 };

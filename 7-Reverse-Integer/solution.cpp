619f237e5736efe2257f8fbb3e9c39163e8f75a0
lhuyuel <hihuyue@gmail.com>
2016-08-02 22:14:19 -0400

    [7. Reverse Integer][Accepted]auto commit

--- 7-Reverse-Integer/solution.cpp
+++ 7-Reverse-Integer/solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int reverse(int x) {
-        
+        if (x == INT_MIN) return 0;
         if (x < 0) return -1 * reverse(-1*x);
         int sum = 0;
         while (x > 0) {
-            if (sum <= INT_MIN/10 || sum >= INT_MAX/10) return 0;
+            if (sum > INT_MAX/10) return 0;
             sum = sum * 10 + x % 10;
             x /= 10;
         }

83f6e034dc45ef225a876481b5eb1d978bf040cc
lhuyuel <hihuyue@gmail.com>
2016-08-03 09:36:26 -0400

    [189. Rotate Array][Runtime Error]auto commit

---
+++ 189-Rotate-Array/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverse(vector<int> &nums, int start, int end) {
+        for (int i = start, j = end; i < j; ++i, --j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        reverse(nums, 0 , n-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, n-1);
+    }
+};

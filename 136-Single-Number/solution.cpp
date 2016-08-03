9a98c12d00223a4f472a710879c3050622244ba3
lhuyuel <hihuyue@gmail.com>
2016-08-03 10:26:56 -0400

    [136. Single Number][Accepted]auto commit

---
+++ 136-Single-Number/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.size(); ++i)
+            result ^= nums[i];
+        return result;
+    }
+};

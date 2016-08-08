9ee1f558a914d63335c79f206eb0e631dc6f66fe
lhuyuel <hihuyue@gmail.com>
2016-08-07 22:35:45 -0400

    [377. Combination Sum IV][Accepted]auto commit

--- 377-Combination-Sum-IV/solution.cpp
+++ 377-Combination-Sum-IV/solution.cpp
@@ -5,7 +5,8 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         int n = nums.size();
         vector<int> dp(target+1,0);
-        for (auto num : nums ) dp[num] = 1;
+        for (int i = 0; i < n; ++i ) 
+            if (nums[i] <= target) dp[nums[i]] = 1;
         for (int i = 0; i <= target; ++i) {
             for (auto num : nums) {
                 if (i > num) dp[i] += dp[i-num];

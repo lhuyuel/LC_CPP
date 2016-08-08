a2fab6392301a44214168aa5a1fb52a4b5d8ee55
lhuyuel <hihuyue@gmail.com>
2016-08-07 22:17:16 -0400

    [377. Combination Sum IV][Runtime Error]auto commit

---
+++ 377-Combination-Sum-IV/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // 就是change Coins吧
+    // dp(n) = dp(n-nums[0]) + dp(n-nums[1]) +...
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(target+1,0);
+        for (auto num : nums ) dp[num] = 1;
+        for (int i = 0; i <= target; ++i) {
+            for (auto num : nums) {
+                if (i > num) dp[i] += dp[i-num];
+            }
+        }
+        return dp[target];
+    }
+};

ccda65191153c9875ce9599a92baa8399f95e2d1
lhuyuel <hihuyue@gmail.com>
2016-08-09 13:31:37 -0400

    [77. Combinations][Accepted]auto commit

--- 77-Combinations/solution.cpp
+++ 77-Combinations/solution.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
     void combination(int n, int k, int start, vector<int> path, vector<vector<int>> &result) {
         if (k == 0) {
             result.push_back(path);
@@ -16,6 +17,27 @@ public:
         vector<vector<int>> result;
         vector<int> path;
         combination(n, k, 1, path, result);
+        return result;
+    }*/
+    
+    vector<vector<int>> result;
+    
+    void getCombine(int n, int k, vector<int> &tmp){
+        if(k ==0 ){
+            result.push_back( tmp );
+            return;
+        }
+        
+        for(int i = n; i >= k; --i ){
+            tmp[k-1] = i;
+            getCombine(i-1, k-1, tmp);
+            
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> tmp(k,0);
+        getCombine(n,k,tmp);
         return result;
     }
 };

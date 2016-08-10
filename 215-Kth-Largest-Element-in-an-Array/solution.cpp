a04c09235dd23571eeb21c7d04faaad3c19d072d
lhuyuel <hihuyue@gmail.com>
2016-08-10 13:30:16 -0400

    [215. Kth Largest Element in an Array][Accepted]auto commit

---
+++ 215-Kth-Largest-Element-in-an-Array/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        make_heap(nums.begin(), nums.end());
+        for (int i = 1; i < k; ++i) {
+            pop_heap(nums.begin(), nums.end());
+            nums.pop_back();
+        }
+        return nums[0];
+    }
+};

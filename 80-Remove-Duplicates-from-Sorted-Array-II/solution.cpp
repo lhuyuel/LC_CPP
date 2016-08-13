4440c20a47b988809b91212f27efc04fc0790cc2
lhuyuel <hihuyue@gmail.com>
2016-08-13 16:00:31 -0400

    [80. Remove Duplicates from Sorted Array II][Accepted]auto commit

--- 80-Remove-Duplicates-from-Sorted-Array-II/solution.cpp
+++ 80-Remove-Duplicates-from-Sorted-Array-II/solution.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if ( n <= 2) return n; 
         int count = 1; // count valid index
         int count2 = 1; // count dups
-        
-        for (int i = 1; i< nums.size(); ++i) {
+        for (int i = 1; i < n; ++i) {
             if (nums[i] == nums[i-1]) 
                 ++count2;
             else count2 = 1;

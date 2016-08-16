52815b63eacde3e57f936430c4b6df13c9ba9fa0
lhuyuel <hihuyue@gmail.com>
2016-08-15 23:11:38 -0400

    [274. H-Index][Accepted]auto commit

--- 274-H-Index/solution.cpp
+++ 274-H-Index/solution.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(), citations.end());
-        reverse(citations.begin(), citations.end());
-        int i = 0;
-        while( i < citations.size() && citations[i] > i) ++i;
-        return i;
+        int i = citations.size()-1, count = 0;
+        while( i >=0 && citations[i] > count) --i, ++count;
+        return count;
     }
 };

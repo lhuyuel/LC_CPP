9cfc7d21aa8b7d6c6cb05101fd447e8e413ca1e9
lhuyuel <hihuyue@gmail.com>
2016-08-17 16:14:46 -0400

    [213. House Robber II][Accepted]auto commit

---
+++ 213-House-Robber-II/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    // DP
+    // f(n) = max(f(n-1), f(n-2) + nums[n]);
+    
+    int linearRob(vector<int> & nums, int start, int end){
+        int prepre = 0, pre = nums[start], val = 0;
+        for (int i = start+1; i < end; ++i) {
+            val = max(prepre + nums[i], pre);
+            prepre = pre, pre = val;
+        }
+        return max(val,pre);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+        return max(linearRob(nums,0, n-1), linearRob(nums,1,n));
+    }
+};

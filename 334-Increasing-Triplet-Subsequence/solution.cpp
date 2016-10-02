cd931305de4d494710853b4264d5cf917abf8a2d
lhuyuel <hihuyue@gmail.com>
2016-10-01 21:46:10 -0400

    [334. Increasing Triplet Subsequence][Accepted]auto commit

---
+++ 334-Increasing-Triplet-Subsequence/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    // keep an array of current increasing sub, size is 3
+    bool increasingTriplet(vector<int>& nums) {
+        int minNum1 = INT_MAX, minNum2 = INT_MAX;
+        for (int i = 0; i < nums.size(); ++i) {
+            // current number is less then the min from before
+            if (nums[i] <= minNum1) minNum1 = nums[i];
+            else {
+                if (nums[i] <= minNum2) minNum2 = nums[i];
+                // means nums[i] > the second smallest number, then it is the third smallest
+                else return true;
+            }
+        }
+        return false;
+    }
+};

72d92d79003611b37b25f7d10fda095da5058b07
lhuyuel <hihuyue@gmail.com>
2016-10-08 13:35:26 -0400

    [5. Longest Palindromic Substring][Accepted]auto commit

--- 5-Longest-Palindromic-Substring/solution.cpp
+++ 5-Longest-Palindromic-Substring/solution.cpp
@@ -3,24 +3,28 @@ public:
     // dp, f[i,j] = if s[i] == s[j] f[i+1,j-1] +2  ,     result+=s[i]
     //                else max(f[i+1,j], f[i,j-1])
     string longestPalindrome(string s) {
-        string result = "";
-        int n = s.length();
-        int maxLen = 0, begin = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n,false));
-        for (int i = 0; i < n; ++i)
-            dp[i][i] = true;
+        if (s.size() < 2)
+            return s;
+        int len = s.size(), max_left = 0, max_len = 1, left, right;
         
-        for (int i = n-1; i >= 0; --i) {
-            for (int j = i; j < n; ++j) {
-                if ( (i + 1 > j -1 || dp[i+1][j-1] ) && s[i] == s[j]) {
-                    dp[i][j] = true;
-                    if (j-i+1 > maxLen) {
-                        maxLen = j-i+1;
-                        begin = i;
-                    }
-                }
+        /* Did some pruning here, if the remainning substring's length is less than current max len, 
+        there is no way to get a longer palindrome string. */
+        int start = 0;
+        while ( start < len && (len-start)*2 > max_len) {
+            left = right = start;
+            //  March through the continueous same characters
+            while (right < len - 1 && s[right + 1] == s[right])
+                ++right;
+            start = right + 1;
+            while (right < len - 1 && left > 0 && s[right + 1] == s[left - 1]) {
+                ++right;
+                --left;
+            }
+            if (max_len < right - left + 1) {
+                max_left = left;
+                max_len = right - left + 1;
             }
         }
-        return s.substr(begin,maxLen);
+        return s.substr(max_left, max_len);
     }
 };

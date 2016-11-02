eb1219da2e0d96f28db0580ce980cb8d84f9426a
lhuyuel <yhu@ringlings-MacBook-Pro-190.local>
2016-11-02 16:01:54 -0400

    backtrack, dsf

---
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.cpp
@@ -0,0 +1,199 @@
+    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }    // backtrack
+    // set up the mapping table for each number
+    vector<string> table = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void dfs(string digits, int cur, string dial, vector<string> &result) {
+        // termination condition
+        if (cur == digits.size() && dial != "") {
+            result.push_back(dial);
+            return;
+        }
+        
+        // try all the options for letter digits[cur]
+        string tmp = table[digits[cur] - '0'];
+        for (int i = 0; i < tmp.size(); ++i)
+            dfs(digits, cur+1, dial + tmp[i], result);
+    }
+
+    vector<string> letterCombinations(string digits) {
+        vector<string> result;
+        string dial = "";
+        dfs(digits, 0, dial, result);
+        return result;
+    }

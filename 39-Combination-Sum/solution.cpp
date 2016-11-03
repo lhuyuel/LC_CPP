32f8763b211b45f93c31f53d3f7db30137c72a42
lhuyuel <yhu@ringlings-MacBook-Pro-190.local>
2016-11-03 13:43:21 -0400

    added remove remove duplication

--- 39-Combination-Sum/solution.cpp
+++ 39-Combination-Sum/solution.cpp
@@ -22,3 +22,40 @@ public:
         return result;
     }
 };
+
+// 20161103 题目修改了，solution不能有重复
+    void combinationSumHelper(vector<int>& candidates, int target, vector<int> &sol,vector<vector<int>> &result) {
+        // termination condition
+        if (target == 0) {
+            result.push_back(sol);
+            return;
+        }
+        
+        // try every options
+        for (auto c: candidates) {
+            // prune
+            if (target - c >= 0) {
+                sol.push_back(c);
+                combinationSumHelper(candidates, target - c, sol, result);
+                sol.pop_back();
+            }
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> sol;
+        vector<vector<int>> result;
+        sort(candidates.begin(), candidates.end());
+        combinationSumHelper(candidates,target, sol, result);
+        if (result.size() != 0) {
+            // remove dup
+            for (auto &v : result) sort(v.begin(), v.end());
+            sort(result.begin(), result.end());
+            
+            int count = 0;
+            for (int i = 1; i < result.size(); ++i)
+                if (result[i] != result[count]) result[++count] = result[i];
+            result.resize(count+1);
+        }
+        return result;
+    }

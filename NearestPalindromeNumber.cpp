7acc9a65de4bd1bc5b1b8af0c9c9c953fd822b01
lhuyuel <ntndeyx@Yues-MBP.home>
2016-11-19 14:07:46 -0500

    添加了相邻三个palindrome的比较

--- NearestPalindromeNumber.cpp
+++ NearestPalindromeNumber.cpp
@@ -1,3 +1,5 @@
+// nearest palindrome
+// test case: 129 should return 131, not 121
 int  nearestPalin(int num) {
     if (num >= 0 && num < 10) return num;
     
@@ -10,15 +12,37 @@ int  nearestPalin(int num) {
     countDgt >>= 1; // check the first half of the number
     int mask_l = 1;
     int result = 0;
+    tmp = num;
     while (countDgt > 0) {
-        int h = num / mask_h;
-        int l = (num / mask_l) % 10;
+        int h = tmp / mask_h;
+        int l = (tmp / mask_l) % 10;
         result += h * mask_h + h * mask_l;
-        num = num - h * mask_h - l * mask_l;
+        tmp = tmp - h * mask_h - l * mask_l;
         mask_h /= 10;
         mask_l *= 10;
         --countDgt;
     }
-    if (isOdd) result += num;
+    // if the number has odd digits, then there are three choices:
+    // mid -1, mid, mid +1 for the middle digit. We need to pick the
+    // one the differs the least form the orginal number
+    if (isOdd) {
+        int result1 = result + tmp;
+        int result2 = result + tmp - mask_l;
+        int result3 = result + tmp + mask_l;
+        
+        int diff1 = abs(result1 - num);
+        int diff2 = abs(result2 - num);
+        int diff3 = abs(result3 - num);
+
+        if (diff1 < diff2) {
+            if (diff1 < diff3) result = result1;
+            else result = result3;
+        }
+        else {
+            if (diff2 < diff3) result = result2;
+            else result = result3;
+        }
+        
+    }
     return result;
 }

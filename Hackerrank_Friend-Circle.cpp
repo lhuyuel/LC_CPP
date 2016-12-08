e89ffdb1fede8fb004eb1b1290f4e2fd8e7e2b1b
lhuyuel <hihuyue@gmail.com>
2016-12-08 15:30:49 -0500

    和island number那题并不一样，确实只是个union find problem。因为是对称的，只需要遍历上半个矩阵

--- Hackerrank_Friend-Circle.cpp
+++ Hackerrank_Friend-Circle.cpp
@@ -1,24 +1,47 @@
 /*
-Problem:
-There are N students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature, i.e., if A is friend of B and B is friend of C, then A is also friend of C. A friend circle is a group of students who are directly or indirectly friends.
+There are  students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature, i.e., if  is friend of  and  is friend of , then  is also friend of . A friend circle is a group of students who are directly or indirectly friends.
 
-You are given a N×N−matrix M which consists of characters Y or N. If M[i][j]=Y, then ith and jth students are friends with each other, otherwise not. You have to print the total number of friend circles in the class.
+You are given a   which consists of characters Y or N. If , then  and students are friends with each other, otherwise not. You have to print the total number of friend circles in the class.
 
 Input Format 
-First line of the input contains an integer N - (size of the matrix), followed by N lines each having N characters.
+First line of the input contains an integer  - (size of the matrix), followed by N lines each having N characters.
 
 Output Format 
 Print the maximum number of friend circles.
 
 Constraints 
-1≤N≤300 
+ 
 Each element of matrix friends will be Y or N. 
 Number of rows and columns will be equal in the matrix.
 
-M[i][i]=Y, where 0≤i<N 
-M[i][j] = M[j][i], where 0≤i<j<N
-*/
+, where  
+ = , where 
 
+Sample Input#00
+
+4
+YYNN
+YYYN
+NYYN
+NNNY
+Sample Output
+
+2
+Explanation: There are two pairs of friends  and . So  is also a pair of friends by transitivity. So first friend circle contains , and second friend circle contains only student .
+
+Sample Input#01
+
+5
+YNNNN
+NYNNN
+NNYNN
+NNNYN
+NNNNY
+Sample Output#01
+
+5
+Explanation: No students are friends with each other. So each friend circle will contain of only one student .
+*/
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -28,36 +51,26 @@ using namespace std;
 /*
     It seems a union find problem, could do a DFS
 */
-vector<vector<int>> directs {{-1,0},{0, 1},{1, 0},{0, -1}};
 
-bool isValid(vector<string> &grid, const vector<int> &cur) {
-    int n = grid.size(), m = grid[0].size();
-    return (cur[0] >= 0 && cur[0] < n && cur[1] >= 0 && cur[1] < m
-       && grid[cur[0]][cur[1]] == 'Y');  
-}
-
-void dfs (vector<string> &grid, const vector<int> &cur) {
-    grid[cur[0]][cur[1]] = 'N'; // mark cur as visited
-    for (int i = 0; i < directs.size(); ++i) {
-        vector<int> nxt(2);
-        nxt[0] =  cur[0] + directs[i][0], nxt[1] = cur[1] + directs[i][1];
-        if (isValid(grid, nxt)) // check if the grid is valid
-            dfs(grid, nxt);
+void dfs (vector<string> &grid, int &cur, vector<bool> &visited) {
+    visited[cur] = true;
+    // find neighbors
+    for (int i = cur +1; i < grid.size(); ++i) {
+        if (grid[cur][i] == 'Y') {
+            dfs(grid, i, visited);
+        }
     }
 }
 
-int countUnion(vector<string> &grid) {
+int countUnion(vector<string> &grid, vector<bool> &visited) {
     int n = grid.size(), m = grid[0].size();
     int count = 0; // count unoins
-    vector<int> cur(2);
+
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (grid[i][j] == 'Y') {
-                cur[0] = i, cur[1] = j;
-                dfs(grid, cur);
-                ++count;
-            }
-        }
+        if (!visited[i]) {
+          dfs(grid,i, visited);  
+          ++count;
+        } 
     }
     return count;
 }
@@ -67,11 +80,11 @@ int main() {
     cin >> n;
     string row;
     vector<string> grid;
+    vector<bool> visited(n, false); 
     while (n--) {
         cin >> row;
         grid.push_back(row);
     }
-    cout << countUnion(grid);
+    cout << countUnion(grid, visited);
     return 0;
 }
-

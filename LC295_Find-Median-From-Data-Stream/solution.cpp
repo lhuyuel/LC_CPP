64e5c6ab65bb1490f4e98f0d36de7396b20eadd6
lhuyuel <huyue@seas.upenn.edu>
2016-12-14 20:02:33 -0500

    minHeap存较大的数，maxHeap存较小的数。

---
+++ LC295_Find-Median-From-Data-Stream/solution.cpp
@@ -0,0 +1,42 @@
+class MedianFinder {
+public:
+    void rebalance(priority_queue<int, vector<int>, less<int>> &min_heap,
+                   priority_queue<int, vector<int>, greater<int>> &max_heap) {
+            // 
+            if (min_heap.size() -  max_heap.size() == 2){
+                int tmp = min_heap.top();
+                min_heap.pop();
+                max_heap.push(tmp);
+            }
+            else if (max_heap.size() - min_heap.size() == 1) {
+                int tmp = max_heap.top();
+                max_heap.pop();
+                min_heap.push(tmp);
+            }
+    }
+
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        if (max_heap.size() == 0) min_heap.push(num); 
+        else {
+            // minHeap stores larger half
+            if (num > max_heap.top()) 
+                max_heap.push(num);    
+            else
+                min_heap.push(num);
+            
+        }
+        rebalance(min_heap, max_heap);
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        if (min_heap.size() == max_heap.size())  // if there are odd number of elements
+            return (min_heap.top() + max_heap.top()) / 2.0;
+        else 
+            return min_heap.top();
+    }
+    private:
+    priority_queue<int, vector<int>, less<int>> min_heap;
+    priority_queue<int, vector<int>, greater<int>> max_heap;
+};

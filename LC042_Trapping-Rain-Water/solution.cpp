26b13082367da886419c5df9c9c6503d7c879069
lhuyuel <huyue@seas.upenn.edu>
2016-12-23 22:15:24 -0500

    left and right pointer, 取较小的一边向中间移动，保证了一定能trap住water。

---
+++ LC042_Trapping-Rain-Water/solution.cpp
@@ -0,0 +1,15 @@
+    int trap(vector<int>& height) {
+        int lmax = 0, rmax= 0;
+        int water = 0;
+        for (int l = 0, r = height.size() - 1; l < r; ) {
+            if (height[l] < height[r]) {
+                lmax = max(height[l],lmax);
+                water += (lmax - height[l++]);
+            }
+            else {
+                rmax = max(height[r], rmax);
+                water += (rmax - height[r--]);
+            }
+        }
+        return water;
+    }

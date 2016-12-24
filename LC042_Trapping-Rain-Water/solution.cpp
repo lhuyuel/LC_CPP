1d26847edc78a108663d53ad7c61c5f3036be86a
lhuyuel <huyue@seas.upenn.edu>
2016-12-23 22:35:16 -0500

    lmax是左边已经经过的最高的bar，rmax是右边已经经过的最高的bar

--- LC042_Trapping-Rain-Water/solution.cpp
+++ LC042_Trapping-Rain-Water/solution.cpp
@@ -1,4 +1,8 @@
-    int trap(vector<int>& height) {
+/*
+	lmax是左边已经经过的最高的bar，rmax是右边已经经过的最高的bar
+*/    
+
+int trap(vector<int>& height) {
         int lmax = 0, rmax= 0;
         int water = 0;
         for (int l = 0, r = height.size() - 1; l < r; ) {
